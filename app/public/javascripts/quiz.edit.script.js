e5212b36b5eb6540945f08027d089c7d98ce89d8
Vinci <vinciwcat@gmail.com>
2012-04-13 23:12:29 +0800

    MC Editor, alpha release
    - Upload image and process to edit
    - Currently handle only 1 file, case for more file need to consider.
    - TODO saving logic in server.
    
    Will not fire pull request in this stage.
    
    Signed-off-by: Vinci <vinciwcat@gmail.com>

--- app/public/javascripts/quiz.edit.script.js
+++ app/public/javascripts/quiz.edit.script.js
@@ -2,65 +2,12 @@ $(function(){
 
 	var dropbox = $('#dropbox'),
 			message = $('.message', dropbox),
+			prefix = (typeof quiz_prefix !== 'undefined') ? quiz_prefix : 'quiz_',
+			sub_prefix = (typeof quiz_sub_prefix !== 'undefined') ? quiz_sub_prefix : 'choice_',
 			delay = 500,
-			url = '/quiz/upload';
+			upload_url = '/quiz/upload',
+			save_url = "/quiz/save";
 			
-	dropbox.filedrop({
-		// The name of the $_FILES entry:
-		'paramname':'files',
-	
-		'maxfiles': 5,
-		'maxfilesize': 2, //in MB
-		'url': url,
-	
-		uploadFinished:function(i,file,res){
-			var f = $.data(file);
-			f.addClass('done');
-			f.find('.uploaded').text("Success!");
-			// response is the JSON object that post_file.php returns
-			callNext(res)
-		},
-	
-		error: function(err, file) {
-			switch(err) {
-				case 'BrowserNotSupported':
-					showMessage('Your browser does not support HTML5 file uploads!');
-					break;
-				case 'TooManyFiles':
-					alert('Too many files! Please select **'+this.maxfiles+'** at most!');
-					break;
-				case 'FileTooLarge':
-					alert(file.name+' is too large! Please upload files up to **'+this.maxfilesize+'MB**');
-					break;
-				default:
-					break;
-			}
-		},
-		beforeEach: function(file){
-			if(!file.type.match(/^image\//)){
-				alert('Only images are allowed!');
-				return false;
-			}
-		},
-		
-		uploadStarted:function(i, file, len){ createImage(file); },
-
-		progressUpdated: function(i, file, progress) {
-			$.data(file).find('.progress .bar').width(progress+"%");
-			if (progress >= 100){
-				var p = $.data(file).find('.progress');
-				setTimeout(function(){
-					p.removeClass('active');
-					setTimeout(function(){
-						p.addClass('progress-success');
-					}, delay);
-				}, delay)
-				
-			}
-			
-		}
-		 
-	});
 
 	var template = '<div class="preview">'+
 						'<span class="imageHolder">'+
@@ -71,6 +18,10 @@ $(function(){
 							'<div class="progress progress-striped active"><div class="bar" style="width:0"></div></div>'+
 						'</div>'+
 					'</div>'; 
+
+	var imgs = [];
+
+	initUploader();
 
 
 	function createImage(file){
@@ -108,129 +59,135 @@ $(function(){
 		message.html(msg);
 	}
 	
-	function callNext(res){
-		console.log(res)
-		$('a.nextstep').show();
+
+	function initUploader(){
 		
+		dropbox.filedrop({
+			// The name of the $_FILES entry:
+			'paramname':'files',
+
+			'maxfiles': 5,
+			'maxfilesize': 2, //in MB
+			'url': upload_url,
+
+			uploadFinished:function(i,file,res){
+				if ( res.status === "success") {	
+					setTimeout(function(){
+						var f = $.data(file);				
+						f.addClass('done');
+						f.find('.uploaded').text("Success!");
+					}, delay*2)
+					imgs.push(res.src);
+				}
+			},
+			afterAll: function(){
+				// response is the JSON object that post_file.php returns
+				callNext(imgs)
+			},
+			error: function(err, file) {
+				switch(err) {
+					case 'BrowserNotSupported':
+						showMessage('Your browser does not support HTML5 file uploads!');
+						break;
+					case 'TooManyFiles':
+						alert('Too many files! Please select **'+this.maxfiles+'** at most!');
+						break;
+					case 'FileTooLarge':
+						alert(file.name+' is too large! Please upload files up to **'+this.maxfilesize+'MB**');
+						break;
+					default:
+						break;
+				}
+			},
+			beforeEach: function(file){
+				if(!file.type.match(/^image\//)){
+					alert('Only images are allowed!');
+					return false;
+				}
+			},
+	
+			uploadStarted:function(i, file, len){ createImage(file); },
+
+			progressUpdated: function(i, file, progress) {
+				$.data(file).find('.progress .bar').width(progress+"%");
+				if (progress >= 100){
+					var p = $.data(file).find('.progress');
+					setTimeout(function(){
+						p.removeClass('active');
+						setTimeout(function(){
+							p.addClass('progress-success');
+						}, delay);
+					}, delay)
+			
+				}
+		
+			}
+	 
+		});
+	}
+	
+	function callNext(imgs){
+		console.log("Processing Uploads...", imgs)
+		$('a.nextstep').show();
+		initNext(imgs);
+	}
+	
+	function initNext(imgs){
+		var url = save_url;
+		var x0 = 120, y0 = 100;
+		var h = 32;
+		var bm = 4;
+		var infos = {};
+		var keys = []
+		var k = 0;
+		
+		var frame_template = '<div class="slide quiz-pane quiz">'+
+														'<div class="quiz quiz-bg"></div>'+
+														'<div class="quiz quiz-wrapper"><div class="quiz quiz-inner"></div></div>'+
+													'</div>';
+		$.each(imgs, function(i, src){
+			
+			var choices = ['A', 'B', 'C', 'D'];
+			var _x = x0, _y = y0;
+			var key = prefix+i;
+			var inf = []
+			$.each(choices, function(j, cho){
+				_x += h + bm;
+				var o = {
+					id: prefix + sub_prefix + k,
+					descr: cho,
+					top: _x,
+					left: _y
+				};
+				inf.push(o);
+				k++;
+			})
+			infos[key] = inf;
+			keys.push(key);
+			
+			var frame = $(frame_template);
+			frame.find('div.quiz-bg').append($("<img>", { 'src': src }))
+			frame.find('div.quiz-inner').attr('id', key);
+			frame.appendTo("#iPicture");
+		})
+		
+		
+		
+		$( "#iPicture" ).iPicture({
+			animation: true,
+			animationBg: "bgblack",
+			animationType: "ltr-slide",
+			pictures: keys,
+			button: "moreblack",
+			moreInfos:infos,
+			modify: true,
+			initialize: false,
+			onSave: function(widget){
+				console.log(widget);
+				var data = JSON.stringify(widget.options.moreInfos);
+				$.post(url, {'layout': data }, function(){ alert("Save success") })
+			}
+		});
 	}
 
-});
-
-$(function(){
-	var url = "/quiz/save";
-	$( "#iPicture" ).iPicture({
-		animation: true,
-		animationBg: "bgblack",
-		animationType: "ltr-slide",
-		pictures: ["picture1","picture2","picture3","picture4","picture5"],
-		button: "moreblack",
-		moreInfos:{					
-			"picture1": [{
-					    "id": "tooltip1",
-					    "descr": "furniture: 299$",
-					    "top": "185px",
-					    "left": "393px"
-					},
-					{
-					    "id": "tooltip2",
-					    "descr": "sofa: 199$",
-					    "top": "346px",
-					    "left": "483px"
-					},
-					{
-					    "id": "tooltip3",
-					    "descr": "silver candle: 2.99$",
-					    "top": "461px",
-					    "left": "556px"
-					}],
-			"picture2": [{
-					    "id": "tooltip4",
-					    "descr": "window",
-					    "top": "71px",
-					    "left": "423px"
-					},
-					{
-					    "id": "tooltip5",
-					    "descr": "basket",
-					    "top": "438px",
-					    "left": "192px"
-					},
-					{
-					    "id": "tooltip6",
-					    "descr": "hoven",
-					    "top": "460px",
-					    "left": "673px"
-					}],
-			"picture3": [{
-					    "id": "tooltip7",
-					    "descr": "Organize the kitchen!",
-					    "top": "391px",
-					    "left": "560px"
-					},
-					{
-					    "id": "tooltip8",
-					    "descr": "Hoven: 399$",
-					    "top": "160px",
-					    "left": "268px"
-					},
-					{
-					    "id": "tooltip9",
-					    "descr": "chest of drawers",
-					    "top": "386px",
-					    "left": "180px"
-					}],
-			"picture4": [{
-					    "id": "tooltip10",
-					    "descr": "pasta maker",
-					    "top": "277px",
-					    "left": "672px"
-					},
-					{
-					    "id": "tooltip11",
-					    "descr": "stool",
-					    "top": "291px",
-					    "left": "281px"
-					},
-					{
-					    "id": "tooltip12",
-					    "descr": "shelf",
-					    "top": "144px",
-					    "left": "579px"
-					},
-					{
-					    "id": "tooltip13",
-					    "descr": "Dishes",
-					    "top": "183px",
-					    "left": "181px"
-					}],
-			"picture5": [{
-					    "id": "tooltip14",
-					    "descr": "bed: 199$",
-					    "top": "398px",
-					    "left": "351px"
-					},
-					{
-					    "id": "tooltip15",
-					    "descr": "asian style lamp",
-					    "top": "146px",
-					    "left": "380px"
-					},
-					{
-					    "id": "tooltip16",
-					    "descr": "console: 105$",
-					    "top": "273px",
-					    "left": "567px"
-					}]},
-		modify: true,
-		initialize: false,
-		onSave: function(widget){
-			console.log(widget);
-			var data = JSON.stringify(widget.options.moreInfos);
-			$.post(url, {'layout': data }, function(){ alert("Save success") })
-		}
-	});
-
-
-
-  
 });
