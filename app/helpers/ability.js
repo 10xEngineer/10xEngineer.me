58c81393782860f890f50174be44bdc19dd97a7b
Katat Choi <kata.choi@gmail.com>
2012-04-20 19:11:03 +0800

    fix some permission bugs

--- app/helpers/ability.js
+++ app/helpers/ability.js
@@ -1,5 +1,15 @@
 var _ = require('underscore');
 
 module.exports.can = function(abilities, action, target, target_id){
-	return _.include(abilities[target][target_id], action);
+	if(abilities.role === 'admin')
+		return true;
+		
+	var actionPermissions; 
+	try{
+		actionPermissions = abilities[target][target_id]
+	}catch()
+	{
+		return false;
+	}
+	return _.include(actionPermissions, action);
 }
