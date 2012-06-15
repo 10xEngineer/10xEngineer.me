df646b6a6bbc420439ddac9e454791d60b713595
Parth Vikani <allinhtml@gmail.com>
2012-06-15 16:13:01 +0530

    Fixed Validation Test

--- app/helpers/validation.js
+++ app/helpers/validation.js
@@ -1,11 +1,33 @@
 module.exports = {
 
 	required : function (value ) {
-		if(value != '') {
-			return true;
+
+		function ltrim(str) { 
+			for(var k = 0; k < str.length && isWhitespace(str.charAt(k)); k++);
+			return str.substring(k, str.length);
+		}
+		function rtrim(str) {
+			for(var j=str.length-1; j>=0 && isWhitespace(str.charAt(j)) ; j--) ;
+			return str.substring(0,j+1);
+		}
+		function trim(str) {
+			return ltrim(rtrim(str));
+		}
+		function isWhitespace(charToCheck) {
+			var whitespaceChars = " \t\n\r\f";
+			return (whitespaceChars.indexOf(charToCheck) != -1);
+		}
+
+		if(typeof(value) != 'undefined') {
+			if(trim(value) != '') {
+				return true;
+			} else {
+				return "is required";			
+			}
 		} else {
 			return "is required";
 		}
+
 	},
 
 	regexp : function(value , regExp) {
@@ -17,7 +39,7 @@ module.exports = {
 	},
 
 	email : function (value) {
-		if(this.regexp(value, new RegExp('[A-Z0-9a-z._%+-]+@[A-Z0-9a-z.-]+.[A-Za-z]{2,4}')) === true) {
+		if(this.regexp(value, new RegExp('^[a-zA-Z0-9._-]+[\\+]{0,1}[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[\\.]{1}[a-zA-Z]{2,6}$')) === true) {
 			return true;
 		} else {
 			return "is an invalid email";
@@ -25,7 +47,7 @@ module.exports = {
 	},
 
 	number : function(value) {
-		if(this.regexp(value, new RegExp('[0-9]+')) === true) {
+		if(value != '' && this.regexp(value, new RegExp('^[0-9]+[.]{0,1}[0-9]*$')) === true) {
 			return true;
 		} else {
 			return "is an invalid number";
