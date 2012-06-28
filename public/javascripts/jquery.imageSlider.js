0e26fefa2304f6770bd8fcef88db85de91ec9915
Parth Vikani <allinhtml@gmail.com>
2012-06-28 20:10:24 +0530

    Image Slider in Course List

---
+++ public/javascripts/jquery.imageSlider.js
@@ -0,0 +1,29 @@
+function slideShow() {
+
+  //Set the opacity of all images to 0
+  $('#gallery div.item').css({opacity: 0.0});
+  
+  //Get the first image and display it (set it to full opacity)
+  $('#gallery div.item:first').css({opacity: 1.0});
+  
+
+  //Call the gallery function to run the slideshow, 3000 = change to next image after 6 seconds
+  setInterval('gallery()',3000);
+  
+}
+
+function gallery() {
+  
+  //if no IMGs have the show class, grab the first image
+  var current = ($('#gallery div.item.show')?  $('#gallery div.item.show') : $('#gallery div.item:first'));
+
+  //Get next image, if it reached the end of the slideshow, rotate it back to the first image
+  var next = ((current.next().length) ? current.next() : $('#gallery div.item:first'));  
+  
+  //Set the fade in effect for the next image, show class has higher z-index
+  next.css({opacity: 0.0}).addClass('show').animate({opacity: 1.0}, 1000);
+
+  //Hide the current image
+  current.animate({opacity: 0.0}, 1000).removeClass('show');
+    
+}
