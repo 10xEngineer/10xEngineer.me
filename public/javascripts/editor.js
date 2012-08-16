a9f41623b4282fb5005efbe9e0c97e57dbe51f9f
Rakshit Menpara <deltasquare4@gmail.com>
2012-08-16 14:57:14 +0530

    Cleaned-up front-end code

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,5 +1,5 @@
 
-define(function(require, exports, module) {
+define('editor', function(require, exports, module) {
 
 require("ace/lib/fixoldbrowsers");
 require("ace/config").init();
@@ -13,8 +13,6 @@ var theme = require("ace/theme/textmate");
 var EditSession = require("ace/edit_session").EditSession;
 var UndoManager = require("ace/undomanager").UndoManager;
 
-var vim = require("ace/keyboard/vim").handler;
-var emacs = require("ace/keyboard/emacs").handler;
 var HashHandler = require("ace/keyboard/hash_handler").HashHandler;
 
 var Renderer = require("ace/virtual_renderer").VirtualRenderer;
@@ -122,65 +120,6 @@ function initDoc(file, path, doc) {
 }
 
 
-function makeHuge(txt) {
-    for (var i = 0; i < 5; i++)
-        txt += txt;
-    return txt
-}
-
-var docs = {
-    "docs/javascript.js": "JavaScript",
-    "docs/clojure.clj": "Clojure",
-    "docs/coffeescript.coffee": "Coffeescript",
-    "docs/coldfusion.cfm": "ColdFusion",
-    "docs/cpp.cpp": "C/C++",
-    "docs/csharp.cs": "C#",
-    "docs/css.css": "CSS",
-    "docs/diff.diff": "Diff",
-    "docs/golang.go": "Go",
-    "docs/groovy.groovy": "Groovy",
-    "docs/Haxe.hx": "haXe",
-    "docs/html.html": "HTML",
-    "docs/java.java": "Java",
-    "docs/json.json": "JSON",
-    "docs/jsx.jsx": "JSX",
-    "docs/latex.tex": {name: "LaTeX", wrapped: true},
-    "docs/less.less": "LESS",
-    "docs/liquid.liquid": "Liquid",
-    "docs/lua.lua": "Lua",
-    "docs/luapage.lp": "LuaPage",
-    "docs/markdown.md": {name: "Markdown", wrapped: true},
-    "docs/ocaml.ml": "OCaml",
-    "docs/OpenSCAD.scad": "OpenSCAD",
-    "docs/perl.pl": "Perl",
-    "docs/pgsql.pgsql": {name: "pgSQL", wrapped: true},
-    "docs/php.php": "PHP",
-    "docs/plaintext.txt": {name: "Plain Text", prepare: makeHuge, wrapped: true},
-    "docs/powershell.ps1": "Powershell",
-    "docs/python.py": "Python",
-    "docs/ruby.rb": "Ruby",
-    "docs/scala.scala": "Scala",
-    "docs/scss.scss": "SCSS",
-    "docs/sh.sh": "SH",
-    "docs/sql.sql": {name: "SQL", wrapped: true},
-    "docs/svg.svg": "SVG",
-    "docs/tcl.tcl": "Tcl",
-    "docs/textile.textile": {name: "Textile", wrapped: true},
-    "docs/xml.xml": "XML",
-    "docs/xquery.xq": "XQuery",
-    "docs/yaml.yaml": "YAML",
-    "docs/c9search.c9search_results": "C9 Search Results"
-}
-
-var ownSource = {
-    /* filled from require*/
-};
-
-var hugeDocs = {
-    "build/src/ace.js": "",
-    "build/src-min/ace.js": ""
-};
-
 if (window.require && window.require.s) try {
     for (var path in window.require.s.contexts._.loaded) {
         if (path.indexOf("!") != -1)
@@ -191,188 +130,39 @@ if (window.require && window.require.s) try {
     }
 } catch(e) {}
 
-function prepareDocList(docs) {
-    var list = []
-    for (var path in docs) {
-        var doc = docs[path];
-        if (typeof doc != "object")
-            doc = {name: doc || path};
-
-        doc.path = path;
-        doc.desc = doc.name.replace(/^(ace|docs|demo|build)\//, "");
-        if (doc.desc.length > 18)
-            doc.desc = doc.desc.slice(0, 7) + ".." + doc.desc.slice(-9)
-
-        fileCache[doc.name] = doc;
-        list.push(doc);
-    };
-
-    return list;
-}
-
-docs = prepareDocList(docs);
-ownSource = prepareDocList(ownSource);
-hugeDocs = prepareDocList(hugeDocs);
 
 /*********** create editor ***************************/
 var container = document.getElementById("editor");
 
 // Splitting.
-var Split = require("ace/split").Split;
-var split = new Split(container, theme, 1);
-env.editor = split.getEditor(0);
-split.on("focus", function(editor) {
-    env.editor = editor;
-    updateUIEditorOptions();
-});
-env.split = split;
+env.editor = newEditor(container);
 window.env = env;
 window.ace = env.editor;
 env.editor.setAnimatedScroll(true);
-
-var consoleEl = dom.createElement("div");
-container.parentNode.appendChild(consoleEl);
-consoleEl.style.position="fixed"
-consoleEl.style.bottom = "1px"
-consoleEl.style.right = 0
-consoleEl.style.background = "white"
-consoleEl.style.border = "1px solid #baf"
-consoleEl.style.zIndex = "100"
-var cmdLine = new singleLineEditor(consoleEl);
-cmdLine.editor = env.editor;
-env.editor.cmdLine = cmdLine;
-
-env.editor.commands.addCommands([{
-    name: "gotoline",
-    bindKey: {win: "Ctrl-L", mac: "Command-L"},
-    exec: function(editor, line) {
-        if (typeof line == "object") {
-            var arg = this.name + " " + editor.getCursorPosition().row;
-            editor.cmdLine.setValue(arg, 1)
-            editor.cmdLine.focus()
-            return
-        }
-        line = parseInt(line, 10);
-        if (!isNaN(line))
-            editor.gotoLine(line);
-    },
-    readOnly: true
-}, {
-    name: "find",
-    bindKey: {win: "Ctrl-F", mac: "Command-F"},
-    exec: function(editor, needle) {
-        if (typeof needle == "object") {
-            var arg = this.name + " " + editor.getCopyText()
-            editor.cmdLine.setValue(arg, 1)
-            editor.cmdLine.focus()
-            return
-        }
-        editor.find(needle);
-    },
-    readOnly: true
-}, {
-    name: "focusCommandLine",
-    bindKey: "shift-esc",
-    exec: function(editor, needle) { editor.cmdLine.focus(); },
-    readOnly: true
-}])
-
-cmdLine.commands.bindKeys({
-    "Shift-Return|Ctrl-Return|Alt-Return": function(cmdLine) { cmdLine.insert("\n")},
-    "Esc|Shift-Esc": function(cmdLine){ cmdLine.editor.focus(); },
-    "Return": function(cmdLine){
-        var command = cmdLine.getValue().split(/\s+/);
-        var editor = cmdLine.editor;
-        editor.commands.exec(command[0], editor, command[1]);
-        editor.focus();
-    },
-})
-
-cmdLine.commands.removeCommands(["find", "gotoline", "findall", "replace", "replaceall"])
-
-/**
- * This demonstrates how you can define commands and bind shortcuts to them.
- */
-
-var commands = env.editor.commands;
-commands.addCommand({
-    name: "save",
-    bindKey: {win: "Ctrl-S", mac: "Command-S"},
-    exec: function() {alert("Fake Save File");}
-});
-
-var keybindings = {
-    // Null = use "default" keymapping
-    ace: null,
-    vim: vim,
-    emacs: emacs,
-    // This is a way to define simple keyboard remappings
-    custom: new HashHandler({
-        "gotoright":      "Tab",
-        "indent":         "]",
-        "outdent":        "[",
-        "gotolinestart":  "^",
-        "gotolineend":    "$"
-     })
-};
 
 
 
 /*********** manage layout ***************************/
 var consoleHight = 20;
 function onResize() {
-    var left = env.split.$container.offsetLeft;
+    var left = env.editor.offsetLeft;
     var width = document.documentElement.clientWidth - left;
     container.style.width = width + "px";
     container.style.height = document.documentElement.clientHeight - consoleHight + "px";
-    env.split.resize();
-
-    consoleEl.style.width = width + "px";
-    cmdLine.resize()
+    env.editor.resize();
 }
 
 window.onresize = onResize;
 onResize();
 
 /*********** options pane ***************************/
-var docEl = document.getElementById("doc");
-var modeEl = document.getElementById("mode");
-var wrapModeEl = document.getElementById("soft_wrap");
-var themeEl = document.getElementById("theme");
-var foldingEl = document.getElementById("folding");
-var selectStyleEl = document.getElementById("select_style");
-var highlightActiveEl = document.getElementById("highlight_active");
-var showHiddenEl = document.getElementById("show_hidden");
-var showGutterEl = document.getElementById("show_gutter");
-var showPrintMarginEl = document.getElementById("show_print_margin");
-var highlightSelectedWordE = document.getElementById("highlight_selected_word");
-var showHScrollEl = document.getElementById("show_hscroll");
-var animateScrollEl = document.getElementById("animate_scroll");
-var softTabEl = document.getElementById("soft_tab");
-var behavioursEl = document.getElementById("enable_behaviours");
 
-var group = document.createElement("optgroup");
-group.setAttribute("label", "Mode Examples");
-fillDropdown(docs, group);
-docEl.appendChild(group);
-var group = document.createElement("optgroup");
-group.setAttribute("label", "Huge documents");
-fillDropdown(hugeDocs, group);
-docEl.appendChild(group);
-var group = document.createElement("optgroup");
-group.setAttribute("label", "own source");
-fillDropdown(ownSource, group);
-docEl.appendChild(group);
-
-
-fillDropdown(modes, modeEl);
-
-bindDropdown("mode", function(value) {
+function loadMode(value) {
     env.editor.getSession().setMode(modesByName[value].mode || modesByName.text.mode);
     env.editor.getSession().modeName = value;
-});
+}
 
-bindDropdown("doc", function(name) {
+function loadContent(name) {
     var doc = fileCache[name];
     if (!doc)
         return;
@@ -399,219 +189,6 @@ bindDropdown("doc", function(name) {
         updateUIEditorOptions();
         env.editor.focus();
     }
-});
-
-function updateUIEditorOptions() {
-    var editor = env.editor;
-    var session = editor.session;
-
-    session.setFoldStyle(foldingEl.value);
-
-    saveOption(docEl, session.name);
-    saveOption(modeEl, session.modeName || "text");
-    saveOption(wrapModeEl, session.getUseWrapMode() ? session.getWrapLimitRange().min || "free" : "off");
-
-    saveOption(selectStyleEl, editor.getSelectionStyle() == "line");
-    saveOption(themeEl, editor.getTheme());
-    saveOption(highlightActiveEl, editor.getHighlightActiveLine());
-    saveOption(showHiddenEl, editor.getShowInvisibles());
-    saveOption(showGutterEl, editor.renderer.getShowGutter());
-    saveOption(showPrintMarginEl, editor.renderer.getShowPrintMargin());
-    saveOption(highlightSelectedWordE, editor.getHighlightSelectedWord());
-    saveOption(showHScrollEl, editor.renderer.getHScrollBarAlwaysVisible());
-    saveOption(animateScrollEl, editor.getAnimatedScroll());
-    saveOption(softTabEl, session.getUseSoftTabs());
-    saveOption(behavioursEl, editor.getBehavioursEnabled());
-}
-
-function saveOption(el, val) {
-    if (!el.onchange && !el.onclick)
-        return;
-
-    if ("checked" in el) {
-        if (val !== undefined)
-            el.checked = val;
-
-        localStorage && localStorage.setItem(el.id, el.checked ? 1 : 0);
-    }
-    else {
-        if (val !== undefined)
-            el.value = val;
-
-        localStorage && localStorage.setItem(el.id, el.value);
-    }
-}
-
-event.addListener(themeEl, "mouseover", function(e){
-    this.desiredValue = e.target.value;
-    if (!this.$timer)
-        this.$timer = setTimeout(this.updateTheme);
-})
-
-event.addListener(themeEl, "mouseout", function(e){
-    this.desiredValue = null;
-    if (!this.$timer)
-        this.$timer = setTimeout(this.updateTheme, 20);
-})
-
-themeEl.updateTheme = function(){
-    env.split.setTheme(themeEl.desiredValue || themeEl.selectedValue);
-    themeEl.$timer = null;
-}
-
-bindDropdown("theme", function(value) {
-    if (!value)
-        return;
-    env.editor.setTheme(value);
-    themeEl.selectedValue = value;
-});
-
-bindDropdown("keybinding", function(value) {
-    env.editor.setKeyboardHandler(keybindings[value]);
-});
-
-bindDropdown("fontsize", function(value) {
-    env.split.setFontSize(value);
-});
-
-bindDropdown("folding", function(value) {
-    env.editor.getSession().setFoldStyle(value);
-    env.editor.setShowFoldWidgets(value !== "manual");
-});
-
-bindDropdown("soft_wrap", function(value) {
-    var session = env.editor.getSession();
-    var renderer = env.editor.renderer;
-    switch (value) {
-        case "off":
-            session.setUseWrapMode(false);
-            renderer.setPrintMarginColumn(80);
-            break;
-        case "40":
-            session.setUseWrapMode(true);
-            session.setWrapLimitRange(40, 40);
-            renderer.setPrintMarginColumn(40);
-            break;
-        case "80":
-            session.setUseWrapMode(true);
-            session.setWrapLimitRange(80, 80);
-            renderer.setPrintMarginColumn(80);
-            break;
-        case "free":
-            session.setUseWrapMode(true);
-            session.setWrapLimitRange(null, null);
-            renderer.setPrintMarginColumn(80);
-            break;
-    }
-});
-
-bindCheckbox("select_style", function(checked) {
-    env.editor.setSelectionStyle(checked ? "line" : "text");
-});
-
-bindCheckbox("highlight_active", function(checked) {
-    env.editor.setHighlightActiveLine(checked);
-});
-
-bindCheckbox("show_hidden", function(checked) {
-    env.editor.setShowInvisibles(checked);
-});
-
-bindCheckbox("display_indent_guides", function(checked) {
-    env.editor.setDisplayIndentGuides(checked);
-});
-
-bindCheckbox("show_gutter", function(checked) {
-    env.editor.renderer.setShowGutter(checked);
-});
-
-bindCheckbox("show_print_margin", function(checked) {
-    env.editor.renderer.setShowPrintMargin(checked);
-});
-
-bindCheckbox("highlight_selected_word", function(checked) {
-    env.editor.setHighlightSelectedWord(checked);
-});
-
-bindCheckbox("show_hscroll", function(checked) {
-    env.editor.renderer.setHScrollBarAlwaysVisible(checked);
-});
-
-bindCheckbox("animate_scroll", function(checked) {
-    env.editor.setAnimatedScroll(checked);
-});
-
-bindCheckbox("soft_tab", function(checked) {
-    env.editor.getSession().setUseSoftTabs(checked);
-});
-
-bindCheckbox("enable_behaviours", function(checked) {
-    env.editor.setBehavioursEnabled(checked);
-});
-
-bindCheckbox("fade_fold_widgets", function(checked) {
-    env.editor.setFadeFoldWidgets(checked);
-});
-
-var secondSession = null;
-bindDropdown("split", function(value) {
-    var sp = env.split;
-    if (value == "none") {
-        if (sp.getSplits() == 2) {
-            secondSession = sp.getEditor(1).session;
-        }
-        sp.setSplits(1);
-    } else {
-        var newEditor = (sp.getSplits() == 1);
-        if (value == "below") {
-            sp.setOrientation(sp.BELOW);
-        } else {
-            sp.setOrientation(sp.BESIDE);
-        }
-        sp.setSplits(2);
-
-        if (newEditor) {
-            var session = secondSession || sp.getEditor(0).session;
-            var newSession = sp.setSession(session, 1);
-            newSession.name = session.name;
-        }
-    }
-});
-
-function bindCheckbox(id, callback) {
-    var el = document.getElementById(id);
-    if (localStorage && localStorage.getItem(id))
-        el.checked = localStorage.getItem(id) == "1";
-
-    var onCheck = function() {
-        callback(!!el.checked);
-        saveOption(el);
-    };
-    el.onclick = onCheck;
-    onCheck();
-}
-
-function bindDropdown(id, callback) {
-    var el = document.getElementById(id);
-    if (localStorage && localStorage.getItem(id))
-        el.value = localStorage.getItem(id);
-
-    var onChange = function() {
-        callback(el.value);
-        saveOption(el);
-    };
-
-    el.onchange = onChange;
-    onChange();
-}
-
-function fillDropdown(list, el) {
-    list.forEach(function(item) {
-        var option = document.createElement("option");
-        option.setAttribute("value", item.name);
-        option.innerHTML = item.desc;
-        el.appendChild(option);
-    });
 }
 
 
@@ -642,12 +219,8 @@ event.addListener(container, "drop", function(e) {
     }
 });
 
-// add multiple cursor support to editor
-require("ace/multi_select").MultiSelect(env.editor);
 
-
-
-function singleLineEditor(el) {
+function newEditor(el) {
     var renderer = new Renderer(el);
     renderer.scrollBar.element.style.display = "none";
     renderer.scrollBar.width = 0;
@@ -663,7 +236,6 @@ function singleLineEditor(el) {
     // todo size change event
     renderer.$computeLayerConfig = function() {
         var longestLine = this.$getLongestLine();
-        var firstRow = 0;
         var lastRow = this.session.getLength();
         var height = this.session.getScreenLength() * this.lineHeight;
 
@@ -671,7 +243,7 @@ function singleLineEditor(el) {
         var config = this.layerConfig;
         config.width = longestLine;
         config.padding = this.$padding;
-        config.firstRow = 0;
+        config.firstRow = 1;
         config.firstRowScreen = 0;
         config.lastRow = lastRow;
         config.lineHeight = this.lineHeight;
@@ -688,16 +260,11 @@ function singleLineEditor(el) {
         this.scroller.style.height = height + "px";
         this.container.style.height = height + "px";
     };
-    renderer.isScrollableBy=function(){return false};
 
     var editor = new Editor(renderer);
     new MultiSelect(editor);
     editor.session.setUndoManager(new UndoManager());
 
-    editor.setHighlightActiveLine(false);
-    editor.setShowPrintMargin(false);
-    editor.renderer.setShowGutter(false);
-    // editor.renderer.setHighlightGutterLine(false);
     return editor;
 };
 
--- public/javascripts/share/chat.js
+++ public/javascripts/share/chat.js
@@ -1,33 +1,5 @@
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
-
-var username;
-if (!(username = readCookie('username'))) {
-	username = randomDocName(4);
-	createCookie('username', username, 5);
-}
+// TODO: Hardcoded for now
+var username = 'test';
 
 $('#message').keydown(function (e) {
 	if ((e.keyCode || e.which) == 13) {
